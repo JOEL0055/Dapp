56df64d40bd617a80a9cd3253d107efb8b5c6f78
JOEL0055 <JOEL0055@e.ntu.edu.sg>
2024-10-25 00:43:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def serve_html():
+    return send_from_directory('Dapp 160426', 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
